a246ae286872a0953cfe9c223753530d1ddf0b94
e2500465 <adilelb2007@gmail.com>
2026-05-18 15:38:40 +0200

    Initialisation du crud

---
+++ voiture/vroum/forms.py
@@ -0,0 +1,34 @@
+from django.forms import ModelForm
+from django.utils.translation import gettext_lazy as _
+from . import models
+
+
+class MarqueForm(ModelForm):
+    class Meta:
+        model = models.Marque
+        fields = ('nom', 'fondateur', 'date_creation', 'pays_origine','resume')
+        labels = {
+            'nom' : _('Nom'),
+            'fondateur' : _('Fondateur') ,
+            'date_creation' : _('Date de creation'),
+            'pays_origine' : _("Pays d'origine"),
+            'resume' : _('Résumé')
+        }
+
+
+class ModelForme(ModelForm):
+    class Meta:
+        model = models.Model
+        fields = ('nom', 'energie', 'nombre_chevaux', 'type_moteur','prix','resume')
+        labels = {
+            'nom' : _('Nom'),
+            'energie' : _('Energie') ,
+            'nombre_chevaux' : _('Nombre de chevaux'),
+            'type_moteur' : _('Type de moteur'),
+            'prix': _('Prix'),
+            'resume' : _('Résumé')
+        }
+
+
+# python manage.py makemigrations vroum
+# python manage.py migrate
--- voiture/vroum/models.py
+++ voiture/vroum/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
 # Create your models here.
+class Marque(models.Model): #déclare la classe Livre héritant de la classe Model, classe
+    nom = models.CharField(max_length=100) # défini un champs de type texte de 100
+
+    fondateur = models.CharField(max_length = 100)
+
+    date_creation = models.DateField(blank=True, null = True) # champs de type date
+
+    pays_origine = models.CharField(blank=False) # champs de type entier devant
+
+    resume = models.TextField(null = True, blank = True) # champs de type text long
+
+    def __str__(self):
+        chaine = f"{self.nom} fondé par {self.fondateur}, créer en {self.date_creation} en {self.pays_origine}"
+        return chaine
+
+
+class Model(models.Model): #déclare la classe Livre héritant de la classe Model, classe
+
+    nom = models.CharField(max_length=100) # défini un champs de type texte de 100
+
+    energie = models.CharField(max_length = 100)
+
+    nombre_chevaux = models.IntegerField(blank=True, null = True) # champs de type date
+
+    type_moteur = models.CharField(blank=False) # champs de type entier devant
+
+    prix = models.IntegerField(blank=False) # champs de type entier devant
+
+    resume = models.TextField(null = True, blank = True) # champs de type text long
+
+    def __str__(self):
+        chaine = f"{self.nom}, {self.energie}, {self.nombre_chevaux}ch, {self.type_moteur}, {self.prix}€"
+        return chaine
