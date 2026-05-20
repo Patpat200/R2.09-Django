08673b0451e648c51390dad066b45f98b70cae24
e2500465 <adilelb2007@gmail.com>
2026-05-20 09:33:09 +0200

    Finalisation du double crud marque et modele

--- voiture/vroum/urls.py
+++ voiture/vroum/urls.py
@@ -3,18 +3,21 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('ajout/', views.ajout),
-    path('traitement/', views.traitement),
-    path('affiche/<int:id>/', views.read),
-    path('', views.all),
-    path('update/<int:id>/', views.traitementupdate),
-    path('delete/<int:id>/', views.delete),
+    path('', views.index),
 
-    path('ajoutmodele/', views.ajoutmodele),
-    path('traitementmodele/', views.traitementmodele),
-    path('2', views.allmodele),
+    path('marque/ajout/', views.ajout),
+    path('marque/traitement/', views.traitement),
+    path('marque/affiche/<int:id>/', views.read),
+    path('marque/', views.all),
+    path('marque/update/<int:id>/', views.traitementupdate),
+    path('marque/delete/<int:id>/', views.delete),
 
-    path('updatemodele/<int:id>/', views.updatemodele),
-    path('deletemodele/<int:id>/', views.deletemodele),
+
+    # path('affichemodele/<int:id>/', views.readmodele),
+    path('modele/ajoutmodele/', views.ajoutmodele),
+    path('modele/traitementmodele/', views.traitementmodele),
+    path('modele/', views.allmodele),
+    path('modele/updatemodele/<int:id>/', views.updatemodele),
+    path('modele/deletemodele/<int:id>/', views.deletemodele),
 
 ]
--- voiture/vroum/views.py
+++ voiture/vroum/views.py
@@ -5,39 +5,48 @@ from django.shortcuts import get_object_or_404
 from .forms import MarqueForm
 from .forms import ModeleForm
 
-# Create your views here.
+
+def index(request):
+    Marque = list(models.Marque.objects.all())
+    Modele = list(models.Modele.objects.all())
+
+    return render(request, "vroum/index.html", {"Marque": Marque, "Modele": Modele})
+
+
+
+
 def ajout(request):
     if request.method == "POST": # arrive en cas de retour sur cette page après une
 
         form = MarqueForm(request)
         if form.is_valid(): # validation du formulaire.
             Marque = form.save() # sauvegarde dans la base
-            return render(request,"vroum/affiche.html",{"Marque" : Marque}) #
+            return render(request,"vroum/marque/affiche.html",{"Marque" : Marque}) #
 
         else:
-            return render(request,"vroum/ajout.html",{"form": form})
+            return render(request,"vroum/marque/ajout.html",{"form": form})
     else :
         form = MarqueForm() # création d'un formulaire vide
-        return render(request,"vroum/ajout.html",{"form" : form})
+        return render(request,"vroum/marque/ajout.html",{"form" : form})
 
 
 def traitement(request):
     lform = MarqueForm(request.POST)
     if lform.is_valid():
         Marque = lform.save()
-        return render(request,"vroum/affiche.html",{"Marque" : Marque})
+        return render(request,"vroum/marque/affiche.html",{"Marque" : Marque})
     else:
-        return render(request,"vroum/ajout.html",{"form": lform})
+        return render(request,"vroum/marque/ajout.html",{"form": lform})
 
 
 def read(request, id):
     Marque = models.Marque.objects.get(pk=id)
-    return render(request,"vroum/affiche.html",{"Marque": Marque})
+    return render(request,"vroum/marque/affiche.html",{"Marque": Marque})
 
 
 def all(request):
     Marque = list(models.Marque.objects.all())
-    return render(request, "vroum/all.html", {"Marque": Marque})
+    return render(request, "vroum/marque/all.html", {"Marque": Marque})
 
 
 def traitementupdate(request, id):
@@ -49,10 +58,10 @@ def traitementupdate(request, id):
             lform.save()
             return HttpResponseRedirect("/vroum/")
         else:
-            return render(request, "vroum/update.html", {"form": lform, "id": id})
+            return render(request, "vroum/marque/update.html", {"form": lform, "id": id})
     else:
         lform = MarqueForm(instance=marque)
-        return render(request, "vroum/update.html", {"form": lform, "id": id})
+        return render(request, "vroum/marque/update.html", {"form": lform, "id": id})
 
 def delete(request, id):
     marque = get_object_or_404(models.Marque, pk=id)
@@ -71,31 +80,31 @@ def ajoutmodele(request):
         form = ModeleForm(request)
         if form.is_valid(): # validation du formulaire.
             Modele = form.save() # sauvegarde dans la base
-            return render(request,"vroum/affichemodele.html",{"Modele" : Modele}) #
+            return render(request,"vroum/modele/affichemodele.html",{"Modele" : Modele}) #
 
         else:
-            return render(request,"vroum/ajoutmodele.html",{"form": form})
+            return render(request,"vroum/modele/ajoutmodele.html",{"form": form})
     else :
         form = ModeleForm() # création d'un formulaire vide
-        return render(request,"vroum/ajoutmodele.html",{"form" : form})
+        return render(request,"vroum/modele/ajoutmodele.html",{"form" : form})
 
 
 def traitementmodele(request):
     lform = ModeleForm(request.POST)
     if lform.is_valid():
         Modele = lform.save()
-        return render(request,"vroum/affichemodele.html",{"Modele" : Modele})
+        return render(request,"vroum/modele/affichemodele.html",{"Modele" : Modele})
     else:
-        return render(request,"vroum/ajoutmodele.html",{"form": lform})
+        return render(request,"vroum/modele/ajoutmodele.html",{"form": lform})
 
 def readmodele(request, id):
     Modele = models.Modele.objects.get(pk=id)
-    return render(request,"vroum/affichemodele.html",{"Modele": Modele})
+    return render(request,"vroum/modele/affichemodele.html",{"Modele": Modele})
 
 
 def allmodele(request):
     Modele = list(models.Modele.objects.all())
-    return render(request, "vroum/allmodele.html", {"Modele": Modele})
+    return render(request, "vroum/modele/allmodele.html", {"Modele": Modele})
 
 
 def updatemodele(request, id):
@@ -105,14 +114,14 @@ def updatemodele(request, id):
         lform = ModeleForm(request.POST, instance=modele)
         if lform.is_valid():
             lform.save()
-            return HttpResponseRedirect("/vroum/2")
+            return HttpResponseRedirect("/vroum/")
         else:
-            return render(request, "vroum/updatemodele.html", {"form": lform, "id": id})
+            return render(request, "vroum/modele/updatemodele.html", {"form": lform, "id": id})
     else:
         lform = ModeleForm(instance=modele)
-        return render(request, "vroum/updatemodele.html", {"form": lform, "id": id})
+        return render(request, "vroum/modele/updatemodele.html", {"form": lform, "id": id})
 
 def deletemodele(request, id):
     modele = get_object_or_404(models.Modele, pk=id)
     modele.delete()
-    return HttpResponseRedirect("/vroum/2")
+    return HttpResponseRedirect("/vroum/")
